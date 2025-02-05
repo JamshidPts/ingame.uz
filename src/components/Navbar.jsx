ba1d8e52e6174057624e0962260b4023e85ae2ce
JamshidPts <jamshidpts14@gmail.com>
2025-02-05 11:58:51 +0500

    changed all navlinks and and did burger menu for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import searchBtn from "../assets/search_btn.svg"
 import korzinaBtn from "../assets/korzina_btn.svg"
 import compareBtn from "../assets/compare_btn.svg"
 import modal_nav from "../assets/mobile_modal.svg"
+import closeBtn from "../assets/close_modal.svg"
 import { NavLink } from 'react-router-dom';
 
 function Navbar() {
@@ -15,19 +16,13 @@ function Navbar() {
                 <div className='flex items-center'>
                     <img src={logo} alt="ingame" className='w-[150px] xl:w-[200px]'/>
                     <div className='hidden sm:hidden text-white items-center lg:flex lg:justify-between cursor-pointer'>
-                        <NavLink className={({ isActive }) => (isActive ? "text-[#D3176D]" : "text-white px-[20px] text-[22px] flex items-center")}>
-                            <a href='#' className='px-[20px] text-[22px] flex items-center'>Продукция <span className='pl-[6px]'><img src={vector} alt="vector" className='w-[19px]'/></span></a>
-                        </NavLink>
-                        <NavLink className={({ isActive }) => (isActive ? "text-[#D3176D]" : "text-white px-[20px] text-[22px] flex items-center")}>
-                            <a href='#' className='px-[20px] text-[22px] flex items-center'>Услуги <span className='pl-[6px]'><img src={vector} alt="vector" className='w-[19px]'/></span></a>
-                        </NavLink>
-                        <NavLink className={({ isActive }) => (isActive ? "text-[#D3176D]" : "text-white px-[20px] text-[22px] flex items-center")}>
-                            <a href='#' className='px-[20px] text-[22px]'>O бренде</a>
-                        </NavLink>
+                        <NavLink className="flex items-center justify-center"><p className='pl-[25px] pr-[5px] text-[22px]'>Продукция</p><img src={vector} alt="vector" className='w-[19px]'/></NavLink>
+                        <NavLink className="flex items-center justify-center"><p className='pl-[25px] pr-[5px] text-[22px]'>Услуги</p><img src={vector} alt="vector" className='w-[19px]'/></NavLink>
+                        <NavLink className="flex items-center justify-center"><p className='pl-[25px] pr-[5px] text-[22px]'>O бренде</p><img src={vector} alt="vector" className='w-[19px]'/></NavLink>
                     </div>
                 </div>
             </div>
-            <div className='text-white flex items-center h-full cursor-pointer gap-[10px] px-[10px]'>
+            <div className='text-white flex items-center h-full cursor-pointer px-[10px]'>
                 <div className='hidden sm:hidden lg:flex lg:justify-between gap-[10px]'>
                     <a className='border border-white py-[3px] px-[8px] text-[18px] font-sans' href="#">Связаться</a>
                     <select className='bg-transparent font-orbitron' name="" id="">
@@ -38,17 +33,29 @@ function Navbar() {
                         <option className='bg-[#0A0A0A]' value="">USZ</option>
                         <option className='bg-[#0A0A0A]' value="">USD</option>
                     </select>
-                    <img className='px-[10px]' src={korzinaBtn} alt="kozina" />
                 </div>
-                <div className='flex justify-between'>
-                    <img className='px-[10px]' src={searchBtn} alt="search" />
-                    <img className='px-[10px]' src={compareBtn} alt="compare" />
+                <div className='flex items-center h-full'>
+                    <img className='p-[10px]' src={compareBtn} alt="compare" />
+                    <img className='p-[10px]' src={searchBtn} alt="search" />
+                    <img className='p-[10px] lg:block hidden sm:hidden' src={korzinaBtn} alt="kozina" />
                 </div>
-                    <img className='lg:hidden' src={modal_nav} alt="modal" />
-                    <div>
-                        
-                    </div>
+                    <img className='lg:hidden w-[50px] p-[10px]' src={modal_nav} alt="modal" />
             </div>
+        </div>
+        <div className='absolute right-0 top-0 text-white flex flex-col items-end lg:hidden bg-[#0F0F0F] min-w-[250px]'>
+            <div className='w-full flex justify-end'>
+                <img src={closeBtn} alt="close" className='w-[75px] px-[26px] py-[30px]'/>
+            </div>
+            <NavLink className="flex items-center justify-between mx-auto border-b-2 border-b-[#252525] w-[200px]">
+                <p className='py-[10px] font-sans text-[19px]'>Продукция</p><img src={vector} alt="vector" className='w-[25px] py-[17px]'/>
+            </NavLink>
+            <NavLink className="flex items-center justify-between mx-auto border-b-2 border-b-[#252525] w-[200px]">
+                <p className='py-[10px] font-sans text-[19px]'>Услуги</p><img src={vector} alt="vector" className='w-[25px] py-[17px]'/>
+            </NavLink>
+            <NavLink className="flex items-center justify-between mx-auto border-b-2 border-b-[#252525] w-[200px]">
+                <p className='py-[10px] font-sans text-[19px]'>O бренде</p><img src={vector} alt="vector" className='w-[25px] py-[17px]'/>
+            </NavLink>
+            
         </div>
     </nav>
   )
