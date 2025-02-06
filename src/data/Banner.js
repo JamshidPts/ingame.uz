287f18d1f1682319a935dc78cd593a01df9315d5
KhamdamovAva <Ava.lifeone@gmail.com>
2025-02-06 12:45:17 +0500

    added Banner.js to data file

---
+++ src/data/Banner.js
@@ -0,0 +1,26 @@
+import PcICon from '../assets/banner/banner_pc.png';
+
+
+export const slides = [
+  {
+    id: 1,
+    title: 'NVIDIA RTX SUPER',
+    description: 'Новые игровые видеокарты NVIDIA GeForce RTX 4070 Super, 4070 Ti и 4080 доступны к заказу!',
+    subText: 'Будь среди первых и протестируй новые возможности',
+    img: PcICon,
+  },
+  {
+    id: 2,
+    title: 'NVIDIA RTX SUPER',
+    description: 'Новые игровые видеокарты NVIDIA GeForce RTX 4070 Super, 4070 Ti и 4080 доступны к заказу!',
+    subText: 'Будь среди первых и протестируй новые возможности',
+    img: PcICon,
+  },
+  {
+    id: 3,
+    title: 'NVIDIA RTX SUPER',
+    description: 'Новые игровые видеокарты NVIDIA GeForce RTX 4070 Super, 4070 Ti и 4080 доступны к заказу!',
+    subText: 'Будь среди первых и протестируй новые возможности',
+    img: PcICon,
+  }
+];
