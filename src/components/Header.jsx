3b27c1557eb1c856c9350c8d18c5716d3257653b
KhamdamovAva <Ava.lifeone@gmail.com>
2025-02-06 12:43:13 +0500

    connected Banner.jsx to Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Banner from './Banner'
 
 function Header() {
   return (
     <header className='m-auto'>
-        <Navbar />
+      <Navbar />
+      <Banner />
     </header>
   )
 }
