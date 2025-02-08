15908bac108a1669d827c7b9ab11895612eac8e0
JamshidPts <jamshidpts14@gmail.com>
2025-02-09 01:10:59 +0500

    created boxShadow and backgroundColor for Novinkiy

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ export default {
       fontFamily: {
         orbitron: ["Orbitron", "sans-serif"], // Добавляем шрифт
       },
+      boxShadow: {
+        'custom-white': '0 0 80px 20px rgba(255, 255, 255, 0.75)',
+      },
+      backgroundColor: {
+        'white-transparent': 'rgba(255, 255, 255, 0.2)',
+      },
     },
   },
   plugins: [],
