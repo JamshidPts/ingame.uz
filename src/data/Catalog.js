954a28168ff82e33ef09a1ce4d392dd915745edf
KhamdamovAva <Ava.lifeone@gmail.com>
2025-02-09 11:55:12 +0500

    created Catalog.js in data

---
+++ src/data/Catalog.js
@@ -0,0 +1,51 @@
+import catalog1 from "../assets/catalog/catalog1.png"
+import catalog2 from "../assets/catalog/catalog2.png"
+import catalog3 from "../assets/catalog/catalog3.png"
+import catalog4 from "../assets/catalog/catalog4.png"
+import catalog5 from "../assets/catalog/catalog5.png"
+import catalog6 from "../assets/catalog/catalog6.png"
+import catalog7 from "../assets/catalog/catalog7.png"
+import catalog8 from "../assets/catalog/catalog8.png"
+
+
+export const CatalogItems = [{
+  id: 1,
+  image: catalog1,
+  text: "Монитор",
+},
+{
+  id: 2,
+  image: catalog2,
+  text: "Столы",
+},
+{
+  id: 3,
+  image: catalog3,
+  text: "Кресла",
+},
+{
+  id: 4,
+  image: catalog4,
+  text: "Клавиатура",
+},
+{
+  id: 5,
+  image: catalog5,
+  text: "Комплектующие",
+},
+{
+  id: 6,
+  image: catalog6,
+  text: "Гарнитура",
+},
+{
+  id: 7,
+  image: catalog7,
+  text: "Мышки",
+},
+{
+  id: 8,
+  image: catalog8,
+  text: "Аксессуары",
+},
+]
