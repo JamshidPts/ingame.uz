ace848ee8cc45f8071eaec4908f48009cced53f6
KhamdamovAva <Ava.lifeone@gmail.com>
2025-02-10 12:07:30 +0500

    updated OurPC.js data

--- src/data/OurPC.js
+++ src/data/OurPC.js
@@ -1,49 +1,74 @@
-import ourPcImg from '../assets/ourPc/images/ourPcImg.png'
-import ourPcIcon1 from '../assets/ourPc/icons/OurPcIcon1.svg'
-import ourPcIcon2 from '../assets/ourPc/icons/OurPcIcon2.svg'
-import ourPcIcon3 from '../assets/ourPc/icons/OurPcIcon3.svg'
-import ourPcIcon4 from '../assets/ourPc/icons/OurPcIcon4.svg'
+import ourPcImg from "../assets/ourPc/images/ourPcImg.png";
+import ourPcIcon1 from "../assets/ourPc/icons/OurPcIcon1.svg";
+import ourPcIcon2 from "../assets/ourPc/icons/OurPcIcon2.svg";
+import ourPcIcon3 from "../assets/ourPc/icons/OurPcIcon3.svg";
+import ourPcIcon4 from "../assets/ourPc/icons/OurPcIcon4.svg";
 
-export const Card = [{
-  id: 1,
-  image: ourPcImg,
-  set: "12 КОМПЛЕКТАЦИЙ",
-  price: "от 5 550 000 сум",
-  discount: "~ 459 000 сум/месяц",
-  heading: "Оптимальные",
-  subtitle: "Сбалансированные  игровые ПК",
-  tableTitle: "Performance",
-  tableName: "Игра/FPS",
-  tableQuality: "1080p",
-  tableSecondQuality: "1440p",
-  game: "GTA V",
-  gameFps: "160",
-  gameFps2: "40",
-}]
-
-export const Icons = [
+export const Card = [
   {
     id: 1,
-    icon: ourPcIcon1,
-    title: "Процессоры",
-    subtitle: "названия процессоров"
+    image: ourPcImg,
+    set: "12 КОМПЛЕКТАЦИЙ",
+    price: "от 5 550 000 сум",
+    discount: "~ 459 000 сум/месяц",
+    heading: "Оптимальные",
+    subtitle: "Сбалансированные игровые ПК",
+    tableTitle: "Performance",
+    tableName: "Игра/FPS",
+    tableQuality: "1080p",
+    tableSecondQuality: "1440p",
+    game: "GTA V",
+    gameFps: "160",
+    gameFps2: "40",
+    icons: [
+      { id: 1, icon: ourPcIcon1, title: "Процессоры", subtitle: "названия процессоров" },
+      { id: 2, icon: ourPcIcon2, title: "Видеокарты", subtitle: "названия" },
+      { id: 3, icon: ourPcIcon3, title: "Охлаждение", subtitle: "названия" },
+      { id: 4, icon: ourPcIcon4, title: "Память", subtitle: "названия" }
+    ]
   },
   {
     id: 2,
-    icon: ourPcIcon2,
-    title: "Видеокарты",
-    subtitle: "названия"
+    image: ourPcImg,
+    set: "12 КОМПЛЕКТАЦИЙ",
+    price: "от 5 550 000 сум",
+    discount: "~ 459 000 сум/месяц",
+    heading: "Оптимальные",
+    subtitle: "Сбалансированные игровые ПК",
+    tableTitle: "Performance",
+    tableName: "Игра/FPS",
+    tableQuality: "1080p",
+    tableSecondQuality: "1440p",
+    game: "GTA V",
+    gameFps: "160",
+    gameFps2: "40",
+    icons: [
+      { id: 1, icon: ourPcIcon1, title: "Процессоры", subtitle: "названия процессоров" },
+      { id: 2, icon: ourPcIcon2, title: "Видеокарты", subtitle: "названия" },
+      { id: 3, icon: ourPcIcon3, title: "Охлаждение", subtitle: "названия" },
+      { id: 4, icon: ourPcIcon4, title: "Память", subtitle: "названия" }
+    ]
   },
   {
     id: 3,
-    icon: ourPcIcon3,
-    title: "Охлаждение",
-    subtitle: "названия"
-  },
-  {
-    id: 4,
-    icon: ourPcIcon4,
-    title: "Память",
-    subtitle: "названия"
+    image: ourPcImg,
+    set: "12 КОМПЛЕКТАЦИЙ",
+    price: "от 5 550 000 сум",
+    discount: "~ 459 000 сум/месяц",
+    heading: "Оптимальные",
+    subtitle: "Сбалансированные игровые ПК",
+    tableTitle: "Performance",
+    tableName: "Игра/FPS",
+    tableQuality: "1080p",
+    tableSecondQuality: "1440p",
+    game: "GTA V",
+    gameFps: "160",
+    gameFps2: "40",
+    icons: [
+      { id: 1, icon: ourPcIcon1, title: "Процессоры", subtitle: "названия процессоров" },
+      { id: 2, icon: ourPcIcon2, title: "Видеокарты", subtitle: "названия" },
+      { id: 3, icon: ourPcIcon3, title: "Охлаждение", subtitle: "названия" },
+      { id: 4, icon: ourPcIcon4, title: "Память", subtitle: "названия" }
+    ]
   }
-]
+];
