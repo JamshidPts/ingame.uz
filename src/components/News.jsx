7a10ca96369d810f4ed556a9b7ea4018f01b1fbe
JamshidPts <jamshidpts14@gmail.com>
2025-02-12 23:07:15 +0500

    updated component changed everything in map

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import { news } from '../data/NewsData'
+import { Link } from 'react-router-dom'
 function News() {
   return (
     <section className='bg-[#1A1A1A] text-white py-[50px]'>
         <div className='container mx-auto min-h-[68vh]'>
             <h2 className='text-[40px] font-[600] mb-[40px] px-4'>Блог и новости</h2>
-            <div className='flex justify-center flex-wrap gap-10 items-center'>
-            {news.map((item) => (
-                <div key={item.id} className='relative'>
-                    <img src={item.img1} alt="image"  className='w-[470px] h-[364px] '/>
-                    <img src={item.img2} alt="image" className='w-[280px] h-[280px] absolute top-[2.9rem] left-[5.7rem] z-10'/>
-                    <p className='text-[33px] font-[600] absolute top-[3rem] left-[4.5rem]'>{item.status}</p>
-                    <p className='text-[40px] font-[500] absolute top-[14.4rem] left-[13.7rem] z-20'>ОТ <span className='font-[700]'>{item.price}</span></p>
-                    <p className='text-[20px] font-[700] bg-[#D3176D] w-[90px] h-[36px] text-center absolute top-[21.6rem] left-[1rem] p-[2px]'>{item.date}</p>
-                    <div className='bg-[#0F0F0F] h-[180px] px-[20px] py-[30px]'>
-                        <p className='text-[20px] font-[600] mb-[10px]'>{item.title}</p>
-                        <p className='w-[380px] text-[14px] pb-[10px]'>{item.descr}</p>
-                        <a href="#" className='text-[14px] text-[#9A9A9A] underline '>{item.url}</a>
+            <div className='flex justify-center lg:justify-between flex-wrap items-center p-4'>
+                {news.map((item) => (
+                    <div key={item.id} className='p-4'>
+                        <div className='relative w-[350px] sm:w-[400px]'>
+                            <img src={item.img1} alt="image" className='w-full'/>
+                            <img src={item.img2} alt="predmeti" className='w-1/2 sm:w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10'/>
+                            <p className='absolute top-[18%] left-[33%] transform -translate-x-1/2 -translate-y-1/2 z-0 font-bold text-[30px]'>{item.status}</p>
+                            <p className='absolute top-[60%] left-[50%] font-bold text-[40px] z-20'>{item.price}</p>
+                            <p className='absolute top-[93%] left-[8%] font-bold text-[18px] bg-[#D3176D] px-2 py-1 border'>{item.date}</p>
+                        </div>
+                        <div className='bg-[#0F0F0F] px-4 min-h-[200px] w-[350px] sm:w-[400px]'>
+                            <h5 className='pt-8 font-bold text-[22px] pb-1'>{item.title}</h5>
+                            <p className='text-[14px] font-medium pb-2'>{item.descr}</p>
+                            <Link to="/" className="text-[#9A9A9A] border-b border-b-[#9A9A9A]">{item.url}</Link>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
             </div>
         </div>
     </section>
