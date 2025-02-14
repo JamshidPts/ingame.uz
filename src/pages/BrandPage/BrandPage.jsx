7eaae8b89ee53c43c49c3a15c5ca8ecd3a63c8fa
KhamdamovAva <Ava.lifeone@gmail.com>
2025-02-14 11:21:08 +0500

    added Choice.jsx and added props

--- src/pages/BrandPage/BrandPage.jsx
+++ src/pages/BrandPage/BrandPage.jsx
@@ -3,6 +3,7 @@ import AboutUs from '../../components/AboutUs'
 import Navbar from '../../components/Navbar'
 import AboutCompany from '../../components/Brand/AboutCompany'
 import AboutUsCom from '../../components/Brand/AboutUsCom'
+import Choice from '../../components/Brand/Choice'
 
 function BrandPage() {
   return (
@@ -12,7 +13,8 @@ function BrandPage() {
       </header>
       <AboutCompany />
       <AboutUsCom />
-      <AboutUs />
+      <Choice />
+      <AboutUs title="Отзывы наших клиентов" text="Об этом лучше всего расскажут сами наши клиенты!" />
     </>
   )
 }
