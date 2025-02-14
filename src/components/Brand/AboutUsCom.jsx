7a26e145ead79d3ac39486205f6b5dd58b6e20de
KhamdamovAva <Ava.lifeone@gmail.com>
2025-02-14 11:20:25 +0500

    changed container width to 1120px

--- src/components/Brand/AboutUsCom.jsx
+++ src/components/Brand/AboutUsCom.jsx
@@ -4,12 +4,12 @@ import aboutUsImg from '../../assets/brand/aboutUs.png'
 function AboutUsCom() {
   return (
     <section className='bg-[#1a1a1a] min-h-[50vh] py-[20px] pb-[60px] text-white'>
-      <div className="container mx-auto w-[1400px] font-orbitron">
+      <div className="container mx-auto w-[1120px] font-orbitron">
         <div className='flex flex-col items-center mb-[40px]'>
           <h2 className='text-[40px] font-[600] max-[510px]:text-[30px] mb-[20px]'>Про нас</h2>
           <p className='w-[130px]  border-[1.6px] border-top border-[#d3176d]'></p>
         </div>
-        <div className='flex items-center justify-center gap-[40px]'>
+        <div className='flex items-center justify-between'>
           <div>
             <img src={aboutUsImg} alt="Image" />
           </div>
@@ -21,7 +21,7 @@ function AboutUsCom() {
               За 7 лет работы мы построили компанию олдним из лучших производителей компьютеров премиум<br></br> класса в Узбекистане;</p>
             <p className='mb-[20px]'>За это время мы собрали мощную команду единомышленников,<br></br>
               с которыми дружим и работаем с самого основания компании.</p>
-            <div className='max-w-[760px] border-[#D3176D] border-[1px] p-[16px]'>
+            <div className='max-w-[752px] border-[#D3176D] border-[1px] p-[16px]'>
               <h4 className='text-white text-[20px]'>Наша миссия</h4>
               <p>Создавать лучшие компьютеры, которые будут дарить геймерам и творческим профессионалам удовольствие от использования.  inGame это восхитительный дизайн, высокая производительность, безупречное качество и персональный сервис</p>
             </div>
