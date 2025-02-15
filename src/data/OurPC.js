d7e08bbcc015ce682cae542510c7e306d49ae465
KhamdamovAva <Ava.lifeone@gmail.com>
2025-02-15 13:05:11 +0500

    added 1 data to Cards

--- src/data/OurPC.js
+++ src/data/OurPC.js
@@ -70,5 +70,27 @@ export const Card = [
       { id: 3, icon: ourPcIcon3, title: "Охлаждение", subtitle: "названия" },
       { id: 4, icon: ourPcIcon4, title: "Память", subtitle: "названия" }
     ]
+  },
+  {
+    id: 4,
+    image: ourPcImg,
+    set: "12 КОМПЛЕКТАЦИЙ",
+    price: "от 5 550 000 сум",
+    discount: "~ 459 000 сум/месяц",
+    heading: "Оптимальные",
+    subtitle: "Сбалансированные игровые ПК",
+    tableTitle: "Performance",
+    tableName: "Игра/FPS",
+    tableQuality: "1080p",
+    tableSecondQuality: "1440p",
+    game: "GTA V",
+    gameFps: "160",
+    gameFps2: "40",
+    icons: [
+      { id: 1, icon: ourPcIcon1, title: "Процессоры", subtitle: "названия процессоров" },
+      { id: 2, icon: ourPcIcon2, title: "Видеокарты", subtitle: "названия" },
+      { id: 3, icon: ourPcIcon3, title: "Охлаждение", subtitle: "названия" },
+      { id: 4, icon: ourPcIcon4, title: "Память", subtitle: "названия" }
+    ]
   }
 ];
