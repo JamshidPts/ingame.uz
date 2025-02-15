7be2aed3c5c173b537c731fc911d4d0d41f8d01f
KhamdamovAva <Ava.lifeone@gmail.com>
2025-02-15 12:21:27 +0500

    added max-w-[1400px]

--- src/components/Brand/AboutCompany.jsx
+++ src/components/Brand/AboutCompany.jsx
@@ -4,7 +4,7 @@ import companyCar from '../../assets/brand/company1.png'
 function AboutCompany() {
   return (
     <section className='bg-[#1a1a1a] min-h-[80vh] sm:min-h-[100vh]  pt-[120px] pb-[50px] text-white'>
-      <div className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 max-w-full">
+      <div className="container mx-auto max-w-[1400px] md:max-w-[1120px] px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 ">
         <div className='font-orbitron mb-[40px]'>
           <h1 className='text-[30px] sm:text-[35px] md:text-[40px] font-[600] mb-[20px]'>
             О компании
