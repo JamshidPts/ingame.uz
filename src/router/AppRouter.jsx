433ddc6db34e51f76c2cfbb03a9d477d32cebf1d
JamshidPts <jamshidpts14@gmail.com>
2025-02-21 17:40:32 +0500

    Services page

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import MainPage from '../pages/MainPage/MainPage'
 import BrandPage from '../pages/BrandPage/BrandPage'
+import ServicesPage from '../pages/Services/ServicesPage'
 
 function AppRouter() {
   return (
@@ -9,6 +10,7 @@ function AppRouter() {
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/brand' element={<BrandPage />} />
+        <Route path='/services' element={<ServicesPage />}/>
       </Routes>
     </>
   )
