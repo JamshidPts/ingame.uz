f9823bcb76b3df9c46a072f775c5be2c4851861c
KhamdamovAva <Ava.lifeone@gmail.com>
2025-02-22 13:04:11 +0500

    Created WeCab.jsx component in Services

---
+++ src/components/Services/WeCan.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function WeCan() {
+  return (
+    <section className='bg-[#1a1a1a] min-h-screen py-[170px] pb-[60px] text-white'>
+      <div className="container font-orbitron mx-auto px-[20px]">
+        <div className='flex flex-col items-center mb-[40px]'>
+          <h2 className=' text-[40px] font-[600] max-[510px]:text-[30px] mb-[20px]'>Что мы можем лучше остальных?</h2>
+          <p className='w-[130px]  border-[1.6px] border-top border-[#d3176d]'></p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default WeCan
