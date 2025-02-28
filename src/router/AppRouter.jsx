077cb6de5f4779868310841dde09aa2063b3c7f5
JamshidPts <jamshidpts14@gmail.com>
2025-02-28 21:02:57 +0500

    added admin page and 404 found page

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,14 +3,17 @@ import { Route, Routes } from 'react-router-dom'
 import MainPage from '../pages/MainPage/MainPage'
 import BrandPage from '../pages/BrandPage/BrandPage'
 import ServicesPage from '../pages/Services/ServicesPage'
+import AdminPage from '../pages/admin/AdminPage'
 
 function AppRouter() {
   return (
     <>
       <Routes>
+        <Route path='/admin' element={<AdminPage />}/>
         <Route path='/' element={<MainPage />} />
         <Route path='/brand' element={<BrandPage />} />
         <Route path='/services' element={<ServicesPage />}/>
+        <Route path="*" element={<h2 className='text-center'>404 Not Found</h2>} />
       </Routes>
     </>
   )
