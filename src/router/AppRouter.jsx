2c60626b3567c1355669676ebecfa8cebea4cfd5
KhamdamovAva <Ava.lifeone@gmail.com>
2025-03-07 12:53:28 +0500

    added Product.jsx way

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,15 +4,17 @@ import MainPage from '../pages/MainPage/MainPage'
 import BrandPage from '../pages/BrandPage/BrandPage'
 import ServicesPage from '../pages/Services/ServicesPage'
 import AdminPage from '../pages/admin/AdminPage'
+import ProductPage from '../pages/ProductPage/ProductPage'
 
 function AppRouter() {
   return (
     <>
       <Routes>
-        <Route path='/admin' element={<AdminPage />}/>
+        <Route path='/admin' element={<AdminPage />} />
         <Route path='/' element={<MainPage />} />
         <Route path='/brand' element={<BrandPage />} />
-        <Route path='/services' element={<ServicesPage />}/>
+        <Route path='/services' element={<ServicesPage />} />
+        <Route path='/products' element={<ProductPage />} />
         <Route path="*" element={<h2 className='text-center'>404 Not Found</h2>} />
       </Routes>
     </>
