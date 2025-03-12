97264d0c3b42172563072fc44e98aeeff4b1b174
KhamdamovAva <Ava.lifeone@gmail.com>
2025-03-12 13:03:15 +0500

    added i18n.js library

---
+++ src/i18n.js
@@ -0,0 +1,31 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import axios from "axios";
+
+const loadTranslations = async (lang) => {
+  try {
+    const response = await axios.get(`https://ingame1.azeme.uz/api/user/langs?lang=${lang}`);
+    return response.data.translations || {};
+  } catch (error) {
+    console.error("Ошибка загрузки переводов:", error);
+    return {};
+  }
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    lng: "ru", // Язык по умолчанию
+    fallbackLng: "en",
+    debug: true,
+    interpolation: { escapeValue: false },
+    resources: {},
+  });
+
+export const changeLanguage = async (lang) => {
+  const translations = await loadTranslations(lang);
+  i18n.addResourceBundle(lang, "translation", translations, true, true);
+  await i18n.changeLanguage(lang);
+};
+
+export default i18n;
