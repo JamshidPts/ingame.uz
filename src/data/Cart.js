48839a8f1017955eaac583117912f1999909c000
KhamdamovAva <Ava.lifeone@gmail.com>
2025-03-12 15:39:49 +0500

    added Cart.js in data

---
+++ src/data/Cart.js
@@ -0,0 +1,34 @@
+import laptop from '../assets/order/orderLaptop.png'
+import trash from '../assets/order/trash.svg'
+
+export const orderInfo = [{
+  id: 1,
+  img: laptop,
+  title: "Название товара",
+  subtitle: "Доп. данные",
+  orderTitle: "Заказ",
+  days: "4-7 дней",
+  trashIcon: trash,
+  price: "25 550 000 сум",
+},
+{
+  id: 2,
+  img: laptop,
+  title: "Название товара",
+  subtitle: "Доп. данные",
+  orderTitle: "Заказ",
+  days: "4-7 дней",
+  trashIcon: trash,
+  price: "25 550 000 сум",
+},
+{
+  id: 3,
+  img: laptop,
+  title: "Название товара",
+  subtitle: "Доп. данные",
+  orderTitle: "Заказ",
+  days: "4-7 дней",
+  trashIcon: trash,
+  price: "25 550 000 сум",
+},
+]
