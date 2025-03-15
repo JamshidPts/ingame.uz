814da5f5b55df15dd4f65e6ea6b049ded0eee88b
JamshidPts <jamshidpts14@gmail.com>
2025-03-15 14:07:46 +0500

    added validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import AppRouter from './router/AppRouter'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 
 
 function App() {
   return (
     <>
+    <ToastContainer
+          style={{ zIndex: 9999 }} 
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          closeOnClick
+          pauseOnHover
+          draggable
+          theme="dark"
+          />
       <AppRouter />
     </>
   )
