8d5ec64015871495b34f48f52e15ce112b92b252
KhamdamovAva <Ava.lifeone@gmail.com>
2025-03-18 13:00:45 +0500

    changed loader timer in AppRouter.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { CartProvider } from './context/CartContext' // ✅ Импортируем провайдер корзины
+import { CartProvider } from './context/CartContext'
 import "./i18n.js";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <CartProvider> {/* ✅ Оборачиваем App */}
+    <CartProvider>
       <App />
     </CartProvider>
   </BrowserRouter>
